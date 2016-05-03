4194f429c2a57a32b2fa040d8a313c8366d1bd55
sorgelig <pour.garbage@gmail.com>
2016-05-03 16:23:42 +0800

    DivMMC: convert to synchroneous version.

--- divmmc.v
+++ divmmc.v
@@ -1,144 +1,152 @@
 // divmmc
 
-module divmmc (
-	input        	clk,
-	input        	nRESET,
+//
+// Total refactoring. Made module synchroneous. (Sorgelig)
+//
 
-	// Bus interface
-	input          enabled,
-	input  [15:0] 	addr,
-	input          nWR,
-	input          nRD,
-	input          nMREQ,
-	input          nIORQ,
-	input          nM1,
-	input  [7:0]  	din,
-	output [7:0]  	dout,
-	
-	output         active,
-	output         active_io,
-	output [18:0]  mapped_addr,
-	
-	output [31:0]  sd_lba,
-	output         sd_rd,
-	output         sd_wr,
-	input			   sd_ack,
-	output		   sd_conf,
-	output		   sd_sdhc,
-	
-	output [7:0]	sd_din,
-	input 		   sd_din_strobe,
-	input  [7:0]   sd_dout,
-	input 		   sd_dout_strobe,
-	
-	output         sd_activity
+
+module divmmc
+(
+	input         clk_sys,
+
+	// CPU interface
+	input         nWR,
+	input         nRD,
+	input         nMREQ,
+	input         nIORQ,
+	input         nM1,
+	input  [15:0] addr,
+	input   [7:0] din,
+	output  [7:0] dout,
+
+	// control
+	input         enable,
+	output        active,
+	output        active_io,
+	output [18:0] mapped_addr,
+
+	// SD/MMC SPI
+	output reg    spi_ss,
+	output        spi_clk,
+   input         spi_di,
+   output        spi_do,
+
+	// SD card access ssignal for LED
+	output        sd_activity
 );
 
-reg [3:0] sram_page = 4'd0;
-//reg       mapram = 1'b0;
-reg       conmem = 1'b0;
+assign    mapped_addr = {((addr[13]) ? {2'b01, sram_page} : 6'b000000), addr[12:0]};
+assign    active      = memactive | conmem;
+assign    active_io   = active & (addr[7:0] == 8'hEB);
+assign    sd_activity = ~spi_ss;
 
-wire io_we = !nIORQ &&  nRD && !nWR &&  nM1;
-wire io_rd = !nIORQ && !nRD &&  nWR &&  nM1;
-wire op_rd = !nMREQ && !nRD &&  nWR && !nM1;
+reg [3:0] sram_page;
+reg       conmem;
+reg       memactive;
 
-assign mapped_addr = {((addr[13]) ? {2'b01, sram_page} : 6'b000000), addr[12:0]};
+wire      io_we = ~nIORQ & ~nWR & nM1;
+wire      io_rd = ~nIORQ & ~nRD & nM1;
+wire      m1    = ~nMREQ & ~nM1;
 
-reg m1_trigger;
-reg memactive = 1'b0;
-assign active = (memactive || conmem);
-assign active_io = (addr[7:0]==8'hEB) && active;
-assign sd_activity = ~sd_cs;
+reg       tx_strobe;
+reg       rx_strobe;
 
-always @(posedge clk) begin
+always @(posedge clk_sys) begin
+	reg old_we, old_rd, old_m1;
+	reg m1_trigger;
 
-	if(!nRESET || !enabled) begin
+	rx_strobe <= 0;
+	tx_strobe <= 0;
 
-		m1_trigger <= 1'b0;
-		memactive <= 1'b0;
-		conmem <= 1'b0;
-		sram_page <= 4'd0;
-		sd_cs <= 1'b1;
-		
-	end else begin
+	if(enable) begin
 
-		spi_rx_strobe <= 1'b0;
-		spi_tx_strobe <= 1'b0;
+		old_we <= io_we;
+		old_rd <= io_rd;
 
 		if(active) begin
-			if(io_we && (addr[7:0] == 8'hE3)) begin 
-				sram_page <= din[3:0];
-				conmem <= din[7];
-				//if(din[6]) mapram <= 1'b1; // can reset only by cycling power (core reload)
+			if(io_we & ~old_we) begin
+				case(addr[7:0])
+					'hE3: {conmem, sram_page} <= {din[7], din[3:0]}; // divmmc memctl
+					'hE7: spi_ss <= din[0];                          // SPI enable
+					'hEB: tx_strobe <= 1'b1;                         // SPI write
+					default:;
+				endcase
 			end
-
-			if(io_we && (addr[7:0] == 8'hE7)) sd_cs <= din[0];
 
 			// SPI read
-			if(io_rd && (addr[7:0] == 8'hEB)) spi_rx_strobe <= 1'b1;
-
-			// SPI write
-			if(io_we && (addr[7:0] == 8'hEB)) spi_tx_strobe <= 1'b1;
+			if(io_rd & ~old_rd & (addr[7:0] == 8'hEB)) rx_strobe <= 1;
 		end
 
-		if(op_rd) begin
-			if((addr==16'h0000) || (addr==16'h0008) || (addr==16'h0038) || (addr==16'h0066) || (addr==16'h04C6) || (addr==16'h0562)) begin
-				// activate automapper after this cycle
-				m1_trigger <= 1'b1;
-			end else if (addr[15:8]==8'h3D) begin
-				// activate automapper immediately
-				memactive <= 1'b1;
-				m1_trigger <= 1'b1;
-			end else if({addr[15:3],3'd0} == 16'h1ff8) begin
-				// deactivate automapper after this cycle
-				m1_trigger <= 1'b0;
-			end
+		old_m1 <= m1;
+		if(m1 & ~old_m1) begin
+			casex(addr)
+				16'h0000, 16'h0008, 16'h0038, 16'h0066, 16'h04C6, 16'h0562: 
+					m1_trigger <= 1;                  // activate automapper after this cycle
+				16'h3DXX: 
+					{memactive, m1_trigger} <= 2'b11; // activate automapper immediately
+				16'b0001111111111XXX:                // 1FF8...1FFF
+					m1_trigger <= 0;                  // deactivate automapper after this cycle
+				default: ;
+			endcase
 		end
+		if(~m1) memactive <= m1_trigger;
 
-		if (nM1==1) memactive <= m1_trigger;
-
+	end else begin
+		m1_trigger <= 0;
+		memactive  <= 0;
+		conmem     <= 0;
+		sram_page  <= 0;
+		spi_ss     <= 1;
 	end
 end
 
-reg spi_tx_strobe;
-reg spi_rx_strobe;
-
-spi sdspi(
-   .clk(clk),
-   .tx_strobe(spi_tx_strobe),
-   .rx_strobe(spi_rx_strobe),
+spi spi
+(
+   .clk_sys(clk_sys),
+   .tx(tx_strobe),
+   .rx(rx_strobe),
    .din(din),
    .dout(dout),
-   
-   .spi_clk(sd_sck),
-   .spi_di(sd_miso),
-   .spi_do(sd_mosi)
+
+   .spi_clk(spi_clk),
+   .spi_di(spi_di),
+   .spi_do(spi_do)
 );
 
-reg   sd_cs;
-wire  sd_sck;
-wire  sd_mosi;
-wire  sd_miso;
+endmodule
 
-sd_card sd_card(
-	.io_lba(sd_lba),
-	.io_rd(sd_rd),
-	.io_wr(sd_wr),
-	.io_ack(sd_ack),
-	.io_conf(sd_conf),
-	.io_sdhc(sd_sdhc),
-	.io_din(sd_dout),
-	.io_din_strobe(sd_dout_strobe),
-	.io_dout(sd_din),
-	.io_dout_strobe(sd_din_strobe),
- 
-	.allow_sdhc(1'b1),  // esxdos supports SDHC
+module spi
+(
+	input        clk_sys,
 
-	.sd_cs(sd_cs),
-	.sd_sck(sd_sck),
-	.sd_sdi(sd_mosi),
-	.sd_sdo(sd_miso)
+	input        tx,        // Byte ready to be transmitted
+	input        rx,        // request to read one byte
+	input  [7:0] din,
+	output [7:0] dout,
+
+	output       spi_clk,
+	input        spi_di,
+	output       spi_do
 );
 
+assign    spi_clk = counter[0];
+assign    spi_do  = io_byte[7]; // data is shifted up during transfer
+assign    dout    = data;
+
+reg [4:0] counter = 5'b10000;  // tx/rx counter is idle
+reg [7:0] io_byte, data;
+
+always @(negedge clk_sys) begin
+	if(counter[4]) begin
+		if(rx | tx) begin
+			counter <= 0;
+			data <= io_byte;
+			io_byte <= tx ? din : 8'hff;
+		end
+	end else begin
+		if(spi_clk) io_byte <= { io_byte[6:0], spi_di };
+		counter <= counter + 2'd1;
+	end
+end
 
 endmodule
