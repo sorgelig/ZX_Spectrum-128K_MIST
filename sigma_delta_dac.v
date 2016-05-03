a53053b33647625827149ff8df7caee9ad071a14
sorgelig <pour.garbage@gmail.com>
2016-05-03 16:32:25 +0800

    Convert project to synchroneous version. Move turbo switch to keyboard: F4-std, F5-7MHz, F6-14MHz.

--- sigma_delta_dac.v
+++ sigma_delta_dac.v
@@ -1,26 +1,25 @@
+//
+// PWM DAC
+//
 
-module sigma_delta_dac #(parameter MSBI=7)(
-   output reg      DACout,   //Average Output feeding analog lowpass
-   input  [MSBI:0] DACin,   //DAC input (excess 2**MSBI)
+module sigma_delta_dac #(parameter MSBI=7)
+(
+   output reg      DACout, //Average Output feeding analog lowpass
+   input  [MSBI:0] DACin,  //DAC input (excess 2**MSBI)
    input           CLK,
    input           RESET
 );
 
-reg [MSBI+2:0] DeltaAdder;   //Output of Delta Adder
-reg [MSBI+2:0] SigmaAdder;   //Output of Sigma Adder
-reg [MSBI+2:0] SigmaLatch;   //Latches output of Sigma Adder
-reg [MSBI+2:0] DeltaB;      //B input of Delta Adder
+reg [MSBI+2:0] DeltaAdder; //Output of Delta Adder
+reg [MSBI+2:0] SigmaAdder; //Output of Sigma Adder
+reg [MSBI+2:0] SigmaLatch; //Latches output of Sigma Adder
+reg [MSBI+2:0] DeltaB;     //B input of Delta Adder
 
-always @ (*)
-   DeltaB = {SigmaLatch[MSBI+2], SigmaLatch[MSBI+2]} << (MSBI+1);
+always @(*) DeltaB = {SigmaLatch[MSBI+2], SigmaLatch[MSBI+2]} << (MSBI+1);
+always @(*) DeltaAdder = DACin + DeltaB;
+always @(*) SigmaAdder = DeltaAdder + SigmaLatch;
 
-always @(*)
-   DeltaAdder = DACin + DeltaB;
-   
-always @(*)
-   SigmaAdder = DeltaAdder + SigmaLatch;
-   
-always @(posedge CLK or posedge RESET)
+always @(posedge CLK or posedge RESET) begin
    if(RESET) begin
       SigmaLatch <= 1'b1 << (MSBI+1);
       DACout <= 1'b0;
@@ -28,4 +27,6 @@ always @(posedge CLK or posedge RESET)
       SigmaLatch <= SigmaAdder;
       DACout <= SigmaLatch[MSBI+2];
    end
-endmodule 
+end
+
+endmodule
--- zxspectrum.sv
+++ zxspectrum.sv
@@ -40,7 +40,6 @@ module zxspectrum
    input         SPI_DI,
    input         SPI_SS2,
    input         SPI_SS3,
-   input         SPI_SS4,
    input         CONF_DATA0,
 
    output [12:0] SDRAM_A,
@@ -57,12 +56,87 @@ module zxspectrum
 );
 `default_nettype none
 
-assign      LED = ~(divmmc_sd_activity | ioctl_erasing | ioctl_download | fdd_read | tape_led);
+assign LED = ~(divmmc_sd_activity | ioctl_erasing | ioctl_download | fdd_read | tape_led);
+
+
+////////////////////   CLOCKS   ///////////////////
+wire clk_sys;
+wire locked;
+
+pll pll
+(
+	.inclk0(CLOCK_27),
+	.c0(clk_sys),
+	.c1(SDRAM_CLK),
+	.locked(locked)
+);
+
+reg  ce_psg;  //1.75MHz
+reg  ce_7mp;
+reg  ce_7mn;
+reg  ce_13k;
+reg  clk_ps2; // ~13.5KHz
+
+reg  cpu_en;
+reg  ce_cpu_tp;
+reg  ce_cpu_tn;
+
+wire ce_cpu_p = cpu_en & cpu_p;
+wire ce_cpu_n = cpu_en & cpu_n;
+wire ce_cpu   = cpu_en & ce_cpu_tp;
+
+wire cpu_p = ~&turbo ? ce_cpu_tp : ce_cpu_sp;
+wire cpu_n = ~&turbo ? ce_cpu_tn : ce_cpu_sn;
+
+always @(negedge clk_sys) begin
+	reg [12:0] counter = 0;
+
+	counter <=  counter + 1'd1;
+
+	ce_7mp  <= !counter[3] & !counter[2:0];
+	ce_7mn  <=  counter[3] & !counter[2:0];
+	ce_psg  <= !counter[5:0];
+	ce_13k  <= !counter[12:0];
+	clk_ps2 <= !counter[12];
+
+	ce_cpu_tp <= !(counter & turbo);
+	ce_cpu_tn <= !((counter & turbo) ^ turbo ^ turbo[4:1]);
+end
+
+reg [4:0] turbo, turbo_req = 5'b11111;
+always @(posedge clk_sys) begin
+	reg f4, f5, f6;
+	{f6,f5,f4} <= Fn[6:4];
+
+	if(!mod) begin
+		if(~f4 & Fn[4]) turbo_req <= 5'b11111;
+		if(~f5 & Fn[5]) turbo_req <= 5'b01111;
+		if(~f6 & Fn[6]) turbo_req <= 5'b00111;
+	end
+end
+
+always @(posedge clk_sys) begin
+	reg [5:0] timeout;
+	reg       old_cen;
+
+	if(timeout) timeout <= timeout + 1'd1;
+
+	old_cen <= cpu_n;
+	if(old_cen & ~cpu_n) begin
+		if(turbo != turbo_req) begin
+			cpu_en  <= 0;
+			timeout <= 1;
+			turbo   <= turbo_req;
+		end else if(!cpu_en && !timeout) begin
+			cpu_en  <= 1;
+		end
+	end
+end
 
 
 //////////////////   MIST ARM I/O   ///////////////////
-wire        PS2_CLK;
-wire        PS2_DAT;
+wire        ps2_kbd_clk;
+wire        ps2_kbd_data;
 
 wire  [7:0] joystick_0;
 wire  [7:0] joystick_1;
@@ -75,38 +149,14 @@ wire [31:0] sd_lba;
 wire        sd_rd;
 wire        sd_wr;
 wire        sd_ack;
-wire        sd_conf;
+wire        sd_ack_conf;
+wire        sd_conf_req;
+wire  [7:0] sd_conf;
 wire        sd_sdhc;
+wire  [9:0] sd_addr;
 wire  [7:0] sd_dout;
-wire        sd_dout_strobe;
 wire  [7:0] sd_din;
-wire        sd_din_strobe;
-
-reg  [10:0] clk14k_div;
-wire        clk_ps2 = clk14k_div[10];
-always @(posedge clk_sys) clk14k_div <= clk14k_div + 1'b1;
-
-user_io #(.STRLEN(125)) user_io
-(
-	.*,
-	.conf_str
-	(
-        "SPECTRUM;TRD;F4,TAP;F1,CSW;O5,Autoload ESXDOS,No,Yes;O2,CPU Speed,3.5MHz,4MHz;O3,Video Type,ZX,Pent;O6,Video Version,48k,128k"
-	),
-
-	// ps2 keyboard emulation
-	.ps2_clk(clk_ps2),				// 12-16khz provided by core
-	.ps2_kbd_clk(PS2_CLK),
-	.ps2_kbd_data(PS2_DAT),
-
-	// unused
-	.joystick_analog_0(),
-	.joystick_analog_1(),
-	.ps2_mouse_clk(),
-	.ps2_mouse_data(),
-	.serial_data(),
-	.serial_strobe()
-);
+wire        sd_din_wr;
 
 wire        ioctl_wr;
 wire [24:0] ioctl_addr;
@@ -114,23 +164,24 @@ wire  [7:0] ioctl_dout;
 wire        ioctl_download;
 wire        ioctl_erasing;
 wire  [4:0] ioctl_index;
-reg         force_erase = 0;
+reg         ioctl_force_erase = 0;
 
-data_io data_io
+mist_io #(.STRLEN(100)) user_io
 (
-	.sck(SPI_SCK),
-	.ss(SPI_SS2),
-	.sdi(SPI_DI),
+	.*,
+	.conf_str
+	(
+        "SPECTRUM;TRD;F1,TAP;F2,CSW;O3,Autoload ESXDOS,No,Yes;O4,Video Type,ZX,Pent;O5,Video Version,48k,128k"
+	),
 
-	.force_erase(force_erase),
-	.downloading(ioctl_download),
-	.erasing(ioctl_erasing),
-	.index(ioctl_index),
+	.ps2_clk(clk_ps2),
 
-	.clk(clk_sys),
-	.wr(ioctl_wr),
-	.addr(ioctl_addr),
-	.dout(ioctl_dout)
+	// unused
+	.joystick_analog_0(),
+	.joystick_analog_1(),
+	.ps2_mouse_clk(),
+	.ps2_mouse_data(),
+	.sd_mounted()
 );
 
 
@@ -144,21 +195,27 @@ wire        nIORQ;
 wire        nRD;
 wire        nWR;
 wire        nRFSH;
-wire        nHALT;
 wire        nBUSACK;
 wire        nINT;
-wire        nWAIT	 = 1;
-wire        nNMI   = esxNMI;
 wire        nBUSRQ = ~(ioctl_download | ioctl_erasing);
-wire        nRESET = locked & ~buttons[1] & ~status[0] & esxRESET & ~(Fn[11] && mod);
+wire        reset  = ~locked | buttons[1] | status[0] | esxRESET | cold_reset | warm_reset | test_reset;
+wire        cold_reset = (mod[1] & Fn[11]) || initRESET;
+wire        warm_reset =  mod[2] & Fn[11];
+wire        test_reset =  mod[0] & Fn[11];
 
-T80a cpu
+// wait for ROM loading
+integer initRESET = 15000;
+always @(posedge clk_sys) if(ce_13k && initRESET) initRESET <= initRESET - 1;
+
+T80pa cpu
 (
-	.RESET_n(nRESET),
-	.CLK_n(clk_cpu),
-	.WAIT_n(nWAIT),
+	.RESET_n(~reset),
+	.CLK(clk_sys),
+	.CEN_p(ce_cpu_p),
+	.CEN_n(ce_cpu_n),
+	.WAIT_n(1),
 	.INT_n(nINT),
-	.NMI_n(nNMI),
+	.NMI_n(~esxNMI),
 	.BUSRQ_n(nBUSRQ),
 	.M1_n(nM1),
 	.MREQ_n(nMREQ),
@@ -166,35 +223,36 @@ T80a cpu
 	.RD_n(nRD),
 	.WR_n(nWR),
 	.RFSH_n(nRFSH),
-	.HALT_n(nHALT),
+	.HALT_n(1),
 	.BUSAK_n(nBUSACK),
 	.A(addr),
 	.DO(cpu_dout),
-	.DI(cpu_din),
-	.RestorePC_n(1),
-	.RestorePC(0),
-	.RestoreINT(0)
+	.DI(cpu_din)
 );
 
 always_comb begin
-	casex({nMREQ, tape_dout_en, ~nM1 | nIORQ | nRD, fdd_sel, divmmc_sel, addr[7:0]==8'h1F})
-		'b00XXXX: cpu_din = ram_dout;
-		'b01XXXX: cpu_din = tape_dout;
-		'b1X01XX: cpu_din = fdd_dout;
-		'b1X001X: cpu_din = divmmc_dout;
-		'b1X0001: cpu_din = {2'b00, joystick_0[5:0] | joystick_1[5:0]};
-		'b1X0000: cpu_din = ula_dout;
-		'b1X1XXX: cpu_din = 8'hFF;
+	casex({nMREQ, tape_dout_en, ~nM1 | nIORQ | nRD, fdd_sel, divmmc_sel, addr[7:0]==8'h1F, addr[0], psg_enable})
+		'b00XXXXXX: cpu_din = ram_dout;
+		'b01XXXXXX: cpu_din = tape_dout;
+		'b1X01XXXX: cpu_din = fdd_dout;
+		'b1X001XXX: cpu_din = divmmc_dout;
+		'b1X0001XX: cpu_din = {2'b00, joystick_0[5:0] | joystick_1[5:0]};
+		'b1X000011: cpu_din = (addr[14] ? sound_data : 8'hFF);
+		'b1X000010: cpu_din = port_ff;
+		'b1X00000X: cpu_din = {1'b1, tape_in, 1'b1, key_data[4:0]};
+		'b1X1XXXXX: cpu_din = 8'hFF;
 	endcase
 end
 
 
 //////////////////   MEMORY   //////////////////
-wire        dma = (~nRESET | ~nBUSACK) & ~nBUSRQ;
+wire        dma = (reset | ~nBUSACK) & ~nBUSRQ;
 reg  [24:0] ram_addr;
 reg   [7:0] ram_din;
 reg         ram_we;
 reg         ram_rd;
+wire  [7:0] ram_dout;
+
 always_comb begin
 	casex({dma, tape_req, fdd_read, ext_ram, addr[15:14]})
 		'b1XXX_XX: ram_addr = ioctl_addr;
@@ -214,24 +272,23 @@ always_comb begin
 	endcase
 
 	casex({dma, tape_req})
-		'b1X: ram_we = ioctl_wr;
-		'b01: ram_we = 0;
-		'b00: ram_we = (ext_ram_write | addr[15] | addr[14]) & ~nMREQ & ~nWR;
-	endcase
-
-	casex({dma, tape_req})
 		'b1X: ram_rd = 0;
 		'b01: ram_rd = ~nMREQ;
 		'b00: ram_rd = (fdd_read | ~nMREQ) & ~nRD;
 	endcase
+
+	casex({dma, tape_req})
+		'b1X: ram_we = ioctl_wr;
+		'b01: ram_we = 0;
+		'b00: ram_we = (ext_ram_write | addr[15] | addr[14]) & ~nMREQ & ~nWR;
+	endcase
 end
 
-wire  [7:0] ram_dout;
 sram ram
 (
 	.*,
 	.init(~locked),
-	.clk_sdram(clk_ram),
+	.clk_sdram(clk_sys),
 	.dout(ram_dout),
 	.din (ram_din),
 	.addr(ram_addr),
@@ -252,88 +309,148 @@ vram vram
     .q(vram_dout)
 );
 
-reg         test_rom;
-reg   [7:0] page_reg     = 0;
-wire        page_disable = page_reg[5];
-wire  [1:0] page_rom     = {~trdos_en & ~test_rom, page_reg[4] & ~test_rom};
-wire        page_scr     = page_reg[3];
-wire  [2:0] page_ram     = page_reg[2:0];
-wire        page_write   = ~nIORQ & ~nWR & nM1 & ~addr[15] & ~addr[1] & ~page_disable;
+reg        test_rom;
+reg  [7:0] page_reg;
+wire       page_disable = page_reg[5];
+wire [1:0] page_rom     = {~trdos_en & ~test_rom, page_reg[4] & ~test_rom};
+wire       page_scr     = page_reg[3];
+wire [2:0] page_ram     = page_reg[2:0];
+wire       page_write   = ~nIORQ & ~nWR & nM1 & ~addr[15] & ~addr[1] & ~page_disable;
 
-always @ (negedge clk_cpu) begin
-	if(~nRESET) begin
+always @(posedge clk_sys) begin
+	reg old_wr;
+	old_wr <= page_write;
+
+	if(reset) begin
 		page_reg <= 0;
 		test_rom <= test_reset;
-	end else if(page_write) begin
-		page_reg <= cpu_dout;
+	end else begin
+		if(page_write & ~old_wr) page_reg <= cpu_dout;
 	end
 end
 
 
-///////////////////   ULA   ///////////////////
-wire        locked;
-wire        clk_cpu;       // CPU clock of 3.5 MHz
-wire        clk_cpu2;      // CPU clock without contention
-wire        clk_ram;       // 84MHz clock for RAM 
-wire        clk_sys;       // 28MHz for system synchronization 
-wire        clk_ula;       // 14MHz
+////////////////////  ULA PORT  ///////////////////
+reg [2:0] border_color;
+reg       ear_out;
+reg       mic_out;
+
+wire ula_we = ~addr[0] & ~(trdos_en | tape_turbo | nIORQ) & ~nWR & nM1;
+always @(posedge clk_sys) begin
+	reg old_we;
+	old_we <= ula_we;
+
+	if(reset) begin
+		border_color <= 0;
+		ear_out <= 0; 
+		mic_out <= 0;
+		old_we  <= 0;
+	end else begin
+		if(ula_we & ~old_we) begin
+			border_color <= cpu_dout[2:0];
+			ear_out <= cpu_dout[4]; 
+			mic_out <= cpu_dout[3];
+		end
+	end
+end
+
+
+////////////////////   AUDIO   ///////////////////
+wire [7:0] sound_data;
+wire [7:0] psg_ch_a;
+wire [7:0] psg_ch_b;
+wire [7:0] psg_ch_c;
+wire       psg_enable = addr[0] & addr[15] & ~addr[1];
+wire       psg_we     = psg_enable & ~nIORQ & ~nWR & nM1;
+
+ym2149 ym2149
+(
+	.CLK(clk_sys),
+	.CE(ce_psg),
+	.RESET(reset),
+	.BDIR(psg_we),
+	.BC(addr[14]),
+	.DI(cpu_dout),
+	.DO(sound_data),
+	.CHANNEL_A(psg_ch_a),
+	.CHANNEL_B(psg_ch_b),
+	.CHANNEL_C(psg_ch_c),
+	.SEL(0),
+	.MODE(0)
+);
+
+sigma_delta_dac #(.MSBI(9)) dac_l
+(
+	.CLK(clk_sys),
+	.RESET(reset),
+	.DACin({1'b0, psg_ch_a, 1'b0} + {2'b00, psg_ch_b} + {2'b00, ear_out, mic_out, tape_in, 5'b00000}),
+	.DACout(AUDIO_L)
+);
+
+sigma_delta_dac #(.MSBI(9)) dac_r
+(
+	.CLK(clk_sys),
+	.RESET(reset),
+	.DACin({1'b0, psg_ch_c, 1'b0} + {2'b00, psg_ch_b} + {2'b00, ear_out, mic_out, tape_in, 5'b00000}),
+	.DACout(AUDIO_R)
+);
+
+
+////////////////////   VIDEO   ///////////////////
+wire        ce_cpu_sn;
+wire        ce_cpu_sp;
 wire [12:0] vram_addr;
 wire  [7:0] vram_dout;
-wire  [7:0] ula_dout;
+wire  [7:0] port_ff;
+video video(.*, .mZX(~status[4]), .m128(status[5]));
+
+
+//////////////////   KEYBOARD   //////////////////
 wire [11:1] Fn;
 wire  [2:0] mod;
-wire        cold_reset = mod[1] & Fn[11];
-wire        test_reset = mod[0] & Fn[11];
-reg         AUDIO_IN;
-
-ula ula( .*, .nIORQ(trdos_en | tape_turbo | nIORQ), .din(cpu_dout), .dout(ula_dout), .turbo(status[2]), .mZX(~status[3]), .m128(status[6]));
+wire  [4:0] key_data;
+keyboard kbd( .* );
 
 
 //////////////////   DIVMMC   //////////////////
-reg   [1:0] esxdos_downloaded = 1'b00;
-wire        esxdos_ready = esxdos_downloaded[~status[5]];
+reg   [1:0] esxdos_downloaded = 2'b00;
+wire        esxdos_ready = esxdos_downloaded[~status[3]];
 wire        ext_ram = divmmc_active && esxdos_ready;
 wire        ext_ram_write = ext_ram && (addr[15:13] == 3'b001);
+
 wire [24:0] divmmc_addr = {6'b000011, divmmc_mapaddr};
-
-wire        esxRESET = ~(esxRQ & ~esxdos_ready & esxdos_downloaded[0]) & !initRESET;
-wire        esxNMI   = ~(esxRQ &  esxdos_ready);
-reg         esxRQ    = 0;
-
-always @(posedge clk_ps2) begin
-	reg sRST1 = 0, sRST2 = 0;
-
-	sRST1 <= (Fn[11] && !mod) | joystick_0[7] | joystick_1[7];
-	sRST2 <= sRST1;
-
-	if(sRST2 & ~sRST1 & ~fdd_ready) esxRQ <= 1;
-		else esxRQ <= 0;
-end
-
-// wait for ESXDOS ROM loading 
-integer initRESET = 32000000;
-always @(posedge clk_sys) if(initRESET) initRESET <= initRESET - 1;
-
-always @(negedge esxRQ, posedge cold_reset) begin
-	if(cold_reset) esxdos_downloaded[1] <= 0;
-		else esxdos_downloaded[1] <= esxdos_downloaded[0];
-end
-
 wire        divmmc_sd_activity;
 wire        divmmc_active;
 wire        divmmc_sel;
 wire [18:0] divmmc_mapaddr;
 wire  [7:0] divmmc_dout;
 
+wire        spi_ss;
+wire        spi_clk;
+wire        spi_di;
+wire        spi_do;
+
+reg         esxRQ;
+wire        esxRESET = esxRQ & ~esxdos_ready & esxdos_downloaded[0];
+wire        esxNMI   = esxRQ &  esxdos_ready;
+wire        btnESX   = ((Fn[11] && !mod) | joystick_0[7] | joystick_1[7]) & ~fdd_ready & ~test_rom;
+
+always @(posedge clk_sys) begin
+	esxRQ <= btnESX;
+	if(esxRQ & ~btnESX) esxdos_downloaded[1] <= esxdos_downloaded[0];
+
+	ioctl_force_erase <= cold_reset;
+	if(cold_reset) esxdos_downloaded[1] <= 0;
+
+	if(ioctl_download && !ioctl_index && (ioctl_addr == 25'h181FFF)) esxdos_downloaded[0] <= 1;
+end
+
 divmmc divmmc
 (
 	.*,
-	.clk(clk_sys),
-
-	.enabled(esxdos_ready),
+	.enable(~reset & esxdos_ready),
 	.din(cpu_dout),
 	.dout(divmmc_dout),
-
 	.active(divmmc_active),
 	.active_io(divmmc_sel),
 	.mapped_addr(divmmc_mapaddr),
@@ -341,17 +458,22 @@ divmmc divmmc
 	.sd_activity(divmmc_sd_activity)
 );
 
-always @(posedge ioctl_wr) if(ioctl_addr == 25'h181fff) esxdos_downloaded[0] <= 1;
-always @(posedge clk_sys) force_erase <= cold_reset;
+sd_card sd_card
+(
+	.*,
+	.allow_sdhc(1),
+	.spi_di(spi_do),
+	.spi_do(spi_di)
+);
 
 
 ///////////////////   FDC   ///////////////////
-reg         trdos_en = 0;
+reg         trdos_en;
 wire  [7:0] wd_dout;
 wire [19:0] fdd_addr;
 wire [19:0] fdd_size;
 wire        fdd_rd;
-reg         fdd_ready = 0;
+reg         fdd_ready;
 reg   [1:0] fdd_drive;
 reg         fdd_side;
 reg         fdd_reset;
@@ -360,12 +482,43 @@ wire        fdd_drq;
 wire        fdd_sel  = trdos_en & addr[2] & addr[1] & ~nIORQ & nM1;
 wire        fdd_read = fdd_rd & fdd_sel;
 wire  [7:0] fdd_dout = addr[7] ? {fdd_intrq, fdd_drq, 6'h3F} : wd_dout;
+wire        fdd_m1   = ~nM1 & ~nMREQ;
+
+always @(posedge clk_sys) begin
+	reg old_wr;
+	reg old_download;
+	reg old_m1;
+
+	old_wr <= nWR;
+	if(old_wr & ~nWR & fdd_sel & addr[7]) {fdd_side, fdd_reset, fdd_drive} <= {~cpu_dout[4], ~cpu_dout[2], cpu_dout[1:0]};
+
+	old_download <= ioctl_download;
+	if(cold_reset) begin
+		fdd_ready <= 0;
+		fdd_size  <= 0;
+	end else begin
+		if(!ioctl_download & old_download & (ioctl_index == 1) & ~esxdos_ready) begin
+			fdd_ready <= 1;
+			fdd_size  <= ioctl_addr[19:0] + 1'b1;
+		end
+	end
+
+	old_m1 <= fdd_m1;
+	if(reset) trdos_en <= 0;
+	else begin
+		if(fdd_m1 && ~old_m1) begin
+			if(addr[15:14]) trdos_en <= 0;
+				else if((addr[13:8] == 6'h3D) & page_rom[0] & fdd_ready) trdos_en <= 1;
+		end
+	end
+end
 
 wd1793 fdd
 (
-	.clk(clk_cpu2),
+	.clk_sys(clk_sys),
+	.ce(ce_cpu),
 	.reset(fdd_reset),
-	.ce(fdd_sel & ~addr[7]),
+	.io_en(fdd_sel & ~addr[7]),
 	.rd(~nRD),
 	.wr(~nWR),
 	.addr(addr[6:5]),
@@ -384,67 +537,39 @@ wd1793 fdd
 	.ready(!fdd_drive & fdd_ready)
 );
 
-always @(posedge clk_cpu2) begin
-	reg old_wr;
-	old_wr <= nWR;
-	if(old_wr & ~nWR & fdd_sel & addr[7]) {fdd_side, fdd_reset, fdd_drive} <= {~cpu_dout[4], ~cpu_dout[2], cpu_dout[1:0]};
-end
-
-always @(negedge ioctl_download, posedge cold_reset) begin
-	if(cold_reset) begin
-		fdd_ready <= 0;
-		fdd_size  <= 0;
-	end else begin
-		if((ioctl_index == 1) & ~esxdos_ready) begin
-			fdd_ready <= 1;
-			fdd_size  <= ioctl_addr[19:0];
-		end
-	end
-end
-
-wire m_pos = ~nM1 & ~nMREQ;
-always @(posedge m_pos, negedge nRESET) begin
-	if(!nRESET) begin
-		trdos_en  <= 0;
-	end else begin
-		if(addr[15:14]) trdos_en <= 0;
-			else if((addr[13:8] == 6'h3D) & page_rom[0] & fdd_ready) trdos_en <= 1;
-	end
-end
-
 
 ///////////////////   TAPE   ///////////////////
 wire [24:0] tape_addr = 25'h400000 + tape_addr_raw;
+wire [24:0] tape_addr_raw;
 wire        tape_req;
 wire        tape_dout_en;
 wire        tape_turbo;
 wire  [7:0] tape_dout;
 wire        tape_led;
+reg         tape_in;
 
-wire [24:0] tape_addr_raw;
 smart_tape tape
 (
-	.reset(~nRESET),
-	.clk(clk_cpu2),
+	.*,
+	.ce(ce_cpu),
 
 	.turbo(tape_turbo),
 	.pause(Fn[1]),
 	.prev(Fn[2]),
 	.next(Fn[3]),
-	.audio_out(AUDIO_IN),
+	.audio_out(tape_in),
 	.activity(tape_led),
 
-	.rd_en(~nRFSH),
-	.rd(tape_req),
-	.addr(tape_addr_raw),
-	.din(ram_dout),
+	.buff_rd_en(~nRFSH),
+	.buff_rd(tape_req),
+	.buff_addr(tape_addr_raw),
+	.buff_din(ram_dout),
 
 	.ioctl_download(ioctl_download & ((ioctl_index == 2) | (ioctl_index == 3))),
-	.ioctl_size(ioctl_addr - 25'h400000),
+	.ioctl_size(ioctl_addr - 25'h400000 + 1'b1),
 	.tap_mode(ioctl_index == 2),
 
-	.cpu_addr(addr),
-	.cpu_m1(~nM1 & ~nMREQ),
+	.m1(~nM1 & ~nMREQ),
 	.rom_en(&page_rom),
 	.dout_en(tape_dout_en),
 	.dout(tape_dout)
