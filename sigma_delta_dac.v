8c7cbb87b7093f0b218663ce2293782a566154af
sorgelig <pour.garbage@gmail.com>
2016-05-07 08:35:19 +0800

    Fix startup click sound.

--- sigma_delta_dac.v
+++ sigma_delta_dac.v
@@ -1,7 +1,8 @@
 //
 // PWM DAC
 //
-
+// MSBI is the highest bit number. NOT amount of bits!
+//
 module sigma_delta_dac #(parameter MSBI=7)
 (
    output reg      DACout, //Average Output feeding analog lowpass
@@ -22,10 +23,10 @@ always @(*) SigmaAdder = DeltaAdder + SigmaLatch;
 always @(posedge CLK or posedge RESET) begin
    if(RESET) begin
       SigmaLatch <= 1'b1 << (MSBI+1);
-      DACout <= 1'b0;
+      DACout <= 1;
    end else begin
       SigmaLatch <= SigmaAdder;
-      DACout <= SigmaLatch[MSBI+2];
+      DACout <= ~SigmaLatch[MSBI+2];
    end
 end
 
